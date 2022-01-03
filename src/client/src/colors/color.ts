05040a6a9f21fce3b8e707aaf445d20537c591cc
Derek <ghdtjrrl94@gmail.com>
2022-01-03 17:34:02 +0900

    feat(atom): Update Button component. (#620)
    - Add emotion styled lib.
    - Update button component.
    - Add color control module.
    - Delete unusing storybook.

---
+++ src/client/src/colors/color.ts
@@ -0,0 +1,69 @@
+import { Color } from "@emotion/react";
+
+const colors: Color = {
+	colors: {
+		default: "#222222",
+	},
+	buttonTextColors: {
+		primary: {
+			default: "rgba(34,34,34,.8)",
+			active: "#222222",
+			hover: "#636363",
+			disabled: "#ffffff",
+		},
+		secondary_buy: {
+			default: "#ffffff",
+			active: "#ffffff",
+			hover: "#ffffff",
+			disabled: "#ffffff",
+		},
+		secondary_sell: {
+			default: "#ffffff",
+			active: "#ffffff",
+			hover: "#ffffff",
+			disabled: "#ffffff",
+		},
+	},
+	buttonBgColors: {
+		primary: {
+			default: "#ffffff",
+			active: "#ffffff",
+			hover: "#eaeaea",
+			disabled: "#ebebeb",
+		},
+		secondary_buy: {
+			default: "#ef6153",
+			active: "#ef6153",
+			hover: "#ef6153",
+			disabled: "#ef6153",
+		},
+		secondary_sell: {
+			default: "#41b979",
+			active: "#41b979",
+			hover: "#41b979",
+			disabled: "#41b979",
+		},
+	},
+	borderColors: {
+		primary: {
+			default: "#d3d3d3",
+			active: "#222222",
+			hover: "#d3d3d3",
+			disabled: "#ebebeb",
+		},
+		secondary_buy: {
+			default: "#ef6153",
+			active: "#ef6153",
+			hover: "#ef6153",
+			disabled: "#ef6153",
+		},
+		secondary_sell: {
+			default: "#41b979",
+			active: "#41b979",
+			hover: "#41b979",
+			disabled: "#41b979",
+		},
+	},
+};
+
+export default colors;
