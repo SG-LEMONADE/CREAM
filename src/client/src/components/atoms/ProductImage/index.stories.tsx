ca053a843032c53336820baf9f898f3fdf0e590a
Derek <ghdtjrrl94@gmail.com>
2022-01-08 15:35:38 +0900

    feat(image): Update products image. (#733)
    - Add product image component.
    - Renders according to category.

---
+++ src/client/src/components/atoms/ProductImage/index.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import ProductImage from "./";
+
+export default {
+	title: "atoms/ProductImage",
+	component: ProductImage,
+} as ComponentMeta<typeof ProductImage>;
+
+const Template: ComponentStory<typeof ProductImage> = (args) => (
+	<ProductImage {...args} />
+);
+
+export const HomeImage = Template.bind({});
+HomeImage.args = {
+	src: "https://kream-phinf.pstatic.net/MjAyMTA3MjhfMjIg/MDAxNjI3NDQxMDA1NjE5.HOgIYywGZaaBJDqUzx2OnX9HAxoOWPvuWHqUn_LZGcgg.VYIuOfA5_GgjBGRowv6dmQuAOPtUvmAxbGpOyUCOCtYg.PNG/p_9d8ed1a74d2540ab9842e63363607bf4.png?type=m",
+	category: "home",
+	backgroundColor: "rgb(235, 240, 245)",
+};
+
+export const ShopImage = Template.bind({});
+ShopImage.args = {
+	src: "https://kream-phinf.pstatic.net/MjAyMTExMTZfMzkg/MDAxNjM3MDQzMzM0MzUz.94K4SoFB9IWLnLRh2iUXjWN0s53ADBEfhwIQQVC_kbAg.4Xl3LXELbnhDdMl8Vz0RdUF7JdQzk_LYyhOHIDvIQUgg.PNG/a_d38d4d9403c34c7c9f4f52bf2ce4f649.png?type=m",
+	category: "shop",
+	backgroundColor: "rgb(246, 238, 237)",
+};
+
+export const ProductItemImage = Template.bind({});
+ProductItemImage.args = {
+	src: "https://kream-phinf.pstatic.net/MjAyMTEyMTBfMjQ4/MDAxNjM5MTEyMTE4ODk3.E5xSNEGfpoeBSUdiEb1TiK0xB8pQZ2sbsmKwir2X0H0g.N-57P9OD7r27nsWNs_0AwgF6CJAO7GrokL22BCeS7QUg.PNG/a_40f9f33ce83747cba477269413bee1f0.png?type=m",
+	category: "products",
+	backgroundColor: "rgb(241, 241, 234)",
+};
---
+++ src/client/src/components/atoms/ProductImage/index.tsx
@@ -0,0 +1,48 @@
+import React, { CSSProperties, FunctionComponent } from "react";
+import Image from "next/image";
+import styled from "@emotion/styled";
+
+type ProductImageProps = {
+	backgroundColor: string;
+	src: string;
+	category: string;
+	style?: CSSProperties;
+};
+
+const imageSizes = {
+	home: "81.5",
+	shop: "81.5",
+	products: "560",
+};
+
+const ProductImage: FunctionComponent<ProductImageProps> = (props) => {
+	const { backgroundColor, category, src } = props;
+	return (
+		<ImageWrapper category={category} backgroundColor={backgroundColor}>
+			<StyledImage category={category} src={src} alt={src} />
+		</ImageWrapper>
+	);
+};
+
+export default ProductImage;
+
+const ImageWrapper = styled.div<{ category: string; backgroundColor: string }>`
+	background-color: ${({ backgroundColor }) => backgroundColor};
+	border-radius: 8px;
+	position: relative;
+	width: ${({ category }) => (category === "products" ? `560px` : `25%`)};
+	padding-top: ${({ category }) => (category === "products" ? `560px` : `25%`)};
+	overflow: hidden;
+`;
+
+const StyledImage = styled(Image)<{ category: string }>`
+	width: ${({ category }) =>
+		category === "products"
+			? `${imageSizes[category]}px`
+			: `${imageSizes[category]}%`};
+	height: auto;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
