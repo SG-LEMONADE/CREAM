5f3d7238b60561f142ed4c149030e8a9ca384414
derek <52649378+Derek-94@users.noreply.github.com>
2022-01-12 13:20:59 +0900

    feat(logo): 로고 컴포넌트 작성.  (#838)
    - 로고 컴포넌트 작성.
    - 2가지 props 받을 수 있음.
      - style, category.

---
+++ src/client/src/components/atoms/Logo/index.tsx
@@ -0,0 +1,14 @@
+import React, { CSSProperties, FunctionComponent } from "react";
+import * as svg from "./svg";
+
+type LogoProps = {
+	category: string;
+	style?: CSSProperties;
+};
+
+const Logo: FunctionComponent<LogoProps> = (props) => {
+	const { category, style } = props;
+	return React.createElement(svg[category], { style });
+};
+
+export default Logo;
