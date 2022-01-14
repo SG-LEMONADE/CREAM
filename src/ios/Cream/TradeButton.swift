e8ca4ba0654272f3e6be196d125114b79ef5230d
KimWanki <seoulkwk95@gmail.com>
2022-01-14 10:58:37 +0900

    feat(UI): 홈뷰 거래 버튼 오토 레이아웃 적용

--- src/ios/Cream/TradeButton.swift
+++ src/ios/Cream/TradeButton.swift
@@ -9,10 +9,7 @@
 import UIKit
 import SnapKit
 
-
-
 final class TradeButton: UIButton {
-    
     enum TradeType: String, CustomStringConvertible {
         case buy, sell
         
@@ -38,15 +35,15 @@ final class TradeButton: UIButton {
     private var price: Int?
     
     init(tradeType: TradeType, price: Int?) {
-        super.init(frame: .zero)
         self.tradeType = tradeType
         self.price = price
-//        applyViewSettings()
+        super.init(frame: .zero)
+        applyViewSettings()
     }
     
     required init?(coder: NSCoder) {
+        applyViewSettings()
         super.init(coder: coder)
-//        applyViewSettings()
     }
     
     private lazy var typeLabel: UILabel = {
@@ -63,4 +60,36 @@ final class TradeButton: UIButton {
         let label = UILabel()
         return label
     }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [priceLabel, detailLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+}
+
+
+extension TradeButton: ViewConfiguration {
+    func buildHierarchy() {
+        self.addSubviews(typeLabel, infoStackView)
+    }
+    
+    func setupConstraints() {
+        typeLabel.snp.makeConstraints {
+            $0.top.equalTo(self.view.snp.top).offset(10)
+            $0.leading.equalTo(self.view.snp.leading).offset(10)
+            $0.trailing.equalTo(self.infoStackView.snp.leading).offset(10)
+            $0.bottom.equalTo(self.view.snp.bottom).offset(-10)
+        }
+        
+        infoStackView.snp.makeConstraints {
+            $0.top.equalTo(typeLabel.snp.top)
+            $0.bottom.equalTo(typeLabel.snp.bottom)
+            $0.trailing.equalTo(self.view.snp.trailing).offset(-10)
+        }
+    }
 }
