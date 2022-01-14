c0b6359b9ba4df7dbdea9387d6ae753b734299c8
KimWanki <seoulkwk95@gmail.com>
2022-01-14 10:26:24 +0900

    feat(UI): 회원가입 뷰 바텀시트 구성

---
+++ src/ios/Cream/Presentation/SignupScene/SizeModalCollectionViewController.swift
@@ -0,0 +1,144 @@
+//
+//  SizeModalCollectionViewController.swift
+//  Cream
+//
+//  Created by wankikim-MN on 2022/01/13.
+//
+
+import UIKit
+
+final class SizeModalCollectionViewController: UIViewController {
+    let maxDimmedAlpha:CGFloat = 0.4
+    
+    private lazy var dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = maxDimmedAlpha
+        return view
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 16
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    // MARK: Container View inner
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Get Started"
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private lazy var exitButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setTitle("X", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(animateDismissView), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var sizeCollectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        
+        return collectionView
+    }()
+    
+    private lazy var notesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue interdum. \n\nUt morbi tincidunt augue interdum velit euismod in pellentesque massa. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Eget nullam non nisi est sit amet. Odio pellentesque diam volutpat commodo. Id eu nisl nunc mi ipsum faucibus vitae.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue interdum. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa."
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var contentStackView: UIStackView = {
+        let spacer = UIView()
+        let stackView = UIStackView(arrangedSubviews: [exitButton, titleLabel, notesLabel, spacer])
+        stackView.axis = .vertical
+        stackView.spacing = 12.0
+        return stackView
+    }()
+    
+    let defaultHeight: CGFloat = UIScreen.main.bounds.size.height * 0.6
+    
+    var containerViewHeightConstraint: NSLayoutConstraint?
+    var containerViewBottomConstraint: NSLayoutConstraint?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+    }
+    
+    func animateShowDimmedView() {
+        dimmedView.alpha = 0
+        UIView.animate(withDuration: 0.4) {
+            self.dimmedView.alpha = self.maxDimmedAlpha
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateShowDimmedView()
+        animatePresentContainer()
+    }
+    
+    func animatePresentContainer() {
+        UIView.animate(withDuration: 0.3) {
+            self.containerViewBottomConstraint?.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func setupView() {
+        view.backgroundColor = .clear
+    }
+    
+    func setupConstraints() {
+        view.addSubview(dimmedView)
+        view.addSubview(containerView)
+        
+        containerView.addSubview(contentStackView)
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
+        dimmedView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 32),
+            contentStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
+            contentStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            contentStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            dimmedView.topAnchor.constraint(equalTo: view.topAnchor),
+            dimmedView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            dimmedView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    
+        containerViewHeightConstraint = containerView.heightAnchor.constraint(equalToConstant: defaultHeight)
+        containerViewBottomConstraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: defaultHeight)
+        
+        containerViewHeightConstraint?.isActive = true
+        containerViewBottomConstraint?.isActive = true
+    }
+    
+    @objc func animateDismissView() {
+        UIView.animate(withDuration: 0.2) {
+            self.containerViewBottomConstraint?.constant = self.defaultHeight
+            self.view.layoutIfNeeded()
+        }
+    
+        dimmedView.alpha = maxDimmedAlpha
+        UIView.animate(withDuration: 0.2) {
+            self.dimmedView.alpha = 0
+        } completion: { _ in
+            self.dismiss(animated: true)
+        }
+    }
+}
---
+++ src/ios/Cream/Presentation/SignupScene/SizeModalCollectionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SizeModalCollectionViewModel.swift
+//  Cream
+//
+//  Created by wankikim-MN on 2022/01/13.
+//
+
+import Foundation
+
+class SizeModalCollectionViewModel {
+    
+}
