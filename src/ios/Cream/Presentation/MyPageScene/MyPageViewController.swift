e43873b0e682c2ad5fc5d36f80130f6e350458f4
KimWanki <seoulkwk95@gmail.com>
2022-01-16 21:42:19 +0900

    feat(UI): 탭바 구성 후 MypageViewController 디폴트 뷰 생성

--- src/ios/Cream/Presentation/MyPageScene/MyPageViewController.swift
+++ src/ios/Cream/Presentation/MyPageScene/MyPageViewController.swift
@@ -6,11 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageViewController: UIViewController {
     
+    private lazy var userTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        applyViewSettings()
+    }
+}
+
+extension MyPageViewController: ViewConfiguration {
+    func buildHierarchy() {
+        view.addSubviews(userTableView)
     }
     
+    func setupConstraints() {
+        userTableView.snp.makeConstraints {
+            $0.top.equalTo(self.view)
+            $0.bottom.equalTo(self.view)
+            $0.leading.equalTo(self.view)
+            $0.trailing.equalTo(self.view)
+        }
+    }
 }
