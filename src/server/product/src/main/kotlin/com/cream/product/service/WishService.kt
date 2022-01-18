ec23cdfa5232f209ff374887c204fcc0c47afc11
KanghoLee <inth8204@gmail.com>
2022-01-18 22:33:38 +0900

    chore(product): 필요없는 import 제거

--- src/server/product/src/main/kotlin/com/cream/product/service/WishService.kt
+++ src/server/product/src/main/kotlin/com/cream/product/service/WishService.kt
@@ -1,11 +1,8 @@
 package com.cream.product.service
 
-import com.cream.product.model.WishEntity
-import com.cream.product.persistence.ProductRepository
 import com.cream.product.persistence.WishRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import javax.transaction.Transactional
 
 @Service
 class WishService {
