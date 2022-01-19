a928da88dda3d8675432f9dd7ffd014358a48adc
KimWanki <seoulkwk95@gmail.com>
2022-01-19 17:09:33 +0900

    feat(UI): 쇼핑 뷰 Header 내 콜렉션 뷰 Dynamic Sizing 구현

--- src/ios/Cream/Presentation/ShopScene/ShopViewFilterHeaderView.swift
+++ src/ios/Cream/Presentation/ShopScene/ShopViewFilterHeaderView.swift
@@ -8,71 +8,23 @@
 import UIKit
 import SnapKit
 
-final class FilterCell: UICollectionViewCell {
-    static let reuseIdentifer = "\(FilterCell.self)"
-    
-    static func fittingSize(availableHeight: CGFloat, data: String) -> CGSize {
-        let cell = FilterCell()
-        cell.configure(data)
-        
-        let targetSize = CGSize(width: UIView.layoutFittingCompressedSize.width, height: availableHeight)
-        print(cell.contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .required))
-        return cell.contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .required)
-    }
-    
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .black
-        label.textAlignment = .center
-        label.showsExpansionTextWhenTruncated = true
-        label.sizeToFit()
-        return label
-    }()
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        self.backgroundColor = .systemGray5
-        layer.cornerRadius = frame.height / 2
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
-        applyViewSettings()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        applyViewSettings()
-    }
-    
-    func configure(_ data: String) {
-        self.titleLabel.text = data
-    }
-}
-
-extension FilterCell: ViewConfiguration {
-    func buildHierarchy() {
-        addSubviews(titleLabel)
-    }
-    
-    func setupConstraints() {
-        titleLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-    }
-}
-
 final class ShopViewFilterHeaderView: UICollectionReusableView {
     static let reuseIdentifier = "\(ShopViewFilterHeaderView.self)"
     
     var numberOfCell = 10
-    let dataSource = ["럭셔리", "스니커즈", "의류", "패션 잡화", "라이프", "테크"]
+    
+    enum Constraint {
+        static let verticalInset: CGFloat = 20
+        static let horizontalInset: CGFloat = 20
+    }
+    
+    let dataSource = ["  ", "럭셔리", " ", "스니커즈", "의류", "패션 잡화", "라이프", "테크"]
     
     lazy var filterCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 5
-        layout.minimumInteritemSpacing = 0
+        layout.minimumInteritemSpacing = 10
         layout.sectionInset = .init(top: 5, left: 5, bottom: 5, right: 5)
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -81,6 +33,7 @@ final class ShopViewFilterHeaderView: UICollectionReusableView {
         
         cv.backgroundColor = .white
         cv.showsHorizontalScrollIndicator = false
+        cv.register(FilterImageCell.self, forCellWithReuseIdentifier: FilterImageCell.reuseIdentifier)
         cv.register(FilterCell.self, forCellWithReuseIdentifier: FilterCell.reuseIdentifer)
         
         return cv
@@ -96,36 +49,63 @@ final class ShopViewFilterHeaderView: UICollectionReusableView {
         applyViewSettings()
     }
     
-    //    func setupDelegate() {
-    //        filterCollectionView.delegate = self
-    //        filterCollectionView.dataSource = self
-    //    }
+    func setupDelegate() {
+        filterCollectionView.delegate = self
+        filterCollectionView.dataSource = self
+    }
 }
 
 
 extension ShopViewFilterHeaderView: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(dataSource.count)
+        
         return dataSource.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         print(#function)
+        if indexPath.item == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterImageCell.reuseIdentifier, for: indexPath) as? FilterImageCell
+            else { return UICollectionViewCell() }
+            
+            cell.configure("slider")
+            return cell
+        }
+        
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterCell.reuseIdentifer, for: indexPath) as? FilterCell
         else {
             return UICollectionViewCell()
         }
         print(dataSource[indexPath.item])
+
         cell.configure(dataSource[indexPath.item])
+        cell.titleLabel.sizeToFit()
         
+        
+        
+        if indexPath.item == 1 {
+            cell.isUserInteractionEnabled = false
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         print(#function)
-//        return FilterCell.fittingSize(availableHeight: 50, data: dataSource[indexPath.item])
-        return CGSize(width: 100, height: 50)
+        guard let cell = filterCollectionView.dequeueReusableCell(withReuseIdentifier: FilterCell.reuseIdentifer,
+                                                                  for: indexPath) as? FilterCell
+        else { return .zero }
+        
+        cell.configure(dataSource[indexPath.item])
+        cell.titleLabel.sizeToFit()
+        
+        var cellWidth = cell.sizeThatFits(cell.titleLabel.frame.size).width + Constraint.horizontalInset
+        let cellHeight = cell.sizeThatFits(cell.titleLabel.frame.size).height + Constraint.verticalInset
+        
+        if indexPath.item == 2 {
+            cellWidth = 2
+        }
+        return CGSize(width: cellWidth, height: cellHeight)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
