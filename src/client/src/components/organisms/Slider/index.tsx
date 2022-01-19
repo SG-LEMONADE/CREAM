e487c8b9e65129513547dbac27d286e06406c124
Derek <ghdtjrrl94@gmail.com>
2022-01-19 12:42:13 +0900

    feat(slider): 슬라이더 컴포넌트 업데이트

--- src/client/src/components/organisms/Slider/index.tsx
+++ src/client/src/components/organisms/Slider/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { Fade } from "react-slideshow-image";
+import { Zoom, Fade } from "react-slideshow-image";
 
 import Icon from "components/atoms/Icon";
 import BannerImage from "components/atoms/BannerImage";
@@ -8,12 +8,13 @@ import "react-slideshow-image/dist/styles.css";
 import styled from "@emotion/styled";
 
 type SliderProps = {
+	productSlider?: boolean;
 	small?: boolean;
 	images: typeof BannerImage[];
 };
 
 const Slider: FunctionComponent<SliderProps> = (props) => {
-	const { small = false, images } = props;
+	const { productSlider = false, small = false, images } = props;
 
 	const properties = {
 		duration: 4000,
@@ -26,25 +27,72 @@ const Slider: FunctionComponent<SliderProps> = (props) => {
 					marginRight: "-30px",
 					zIndex: "99",
 					cursor: "pointer",
+					width: "10px",
 				}}
 			/>
 		),
 		nextArrow: (
 			<Icon
 				name="ChevronRight"
-				style={{ marginLeft: "-30px", zIndex: "99", cursor: "pointer" }}
+				style={{
+					marginLeft: "-30px",
+					zIndex: "99",
+					cursor: "pointer",
+					width: "10px",
+				}}
 			/>
 		),
 	};
 
+	const productProperties = {
+		duration: 4000,
+		transitionDuration: 400,
+		infinite: true,
+		prevArrow: (
+			<Icon
+				name="ChevronLeft"
+				style={{
+					marginRight: "-30px",
+					zIndex: "99",
+					cursor: "pointer",
+					width: "10px",
+				}}
+			/>
+		),
+		nextArrow: (
+			<Icon
+				name="ChevronRight"
+				style={{
+					marginLeft: "-30px",
+					zIndex: "99",
+					cursor: "pointer",
+					width: "10px",
+				}}
+			/>
+		),
+		scale: 1.5,
+	};
+
 	return (
-		<StyledFade small={small} {...properties}>
-			{images.map((Image, index) => (
-				<div key={index} className="each-fade">
-					<>{Image}</>
-				</div>
-			))}
-		</StyledFade>
+		<>
+			{productSlider ? (
+				<StyledSlider {...productProperties}>
+					{images.map((Image, index) => (
+						<div key={index} className="each-slide">
+							<>{Image}</>
+						</div>
+					))}
+				</StyledSlider>
+			) : (
+				<StyledFade small={small} {...properties}>
+					{images.map((Image, index) => (
+						<div key={index} className="each-fade">
+							<>{Image}</>
+						</div>
+					))}
+				</StyledFade>
+			)}
+		</>
 	);
 };
 
@@ -52,4 +100,8 @@ export default Slider;
 
 const StyledFade = styled(Fade)<{ small: boolean }>`
 	max-width: ${({ small }) => small && "1200px"};
+`;
+
+const StyledSlider = styled(Zoom)`
+	max-width: 560px;
 `;
