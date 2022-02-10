3f079ddcd9ebe1ed36d05e3e67c2bf151bd71803
deserve82 <inth8204@gmail.com>
2022-02-10 09:20:37 +0900

    fix(product): 거래내역 상품이미지 목록 배열로 반환하기

--- src/server/product/src/main/kotlin/com/cream/product/dto/tradeDTO/TradeHistoryDTO.kt
+++ src/server/product/src/main/kotlin/com/cream/product/dto/tradeDTO/TradeHistoryDTO.kt
@@ -1,13 +1,14 @@
 package com.cream.product.dto.tradeDTO
 
 import com.cream.product.constant.TradeStatus
+import com.fasterxml.jackson.annotation.JsonRawValue
 import com.querydsl.core.annotations.QueryProjection
 import java.time.LocalDateTime
 
 data class TradeHistoryDTO @QueryProjection constructor(
     val name: String,
     val size: String,
-    val imageUrl: String,
+    @JsonRawValue val imageUrl: String,
     val backgroundColor: String,
     val tradeStatus: TradeStatus,
     val updateDateTime: LocalDateTime?,
